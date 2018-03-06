f71a91802f026dad58b6f9b69cbfeb0b07f9ee6c
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2018-03-05 22:08:18 -0300

    Fazendo o app aproveitar o usuário logado na tela inicial

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -20,7 +20,16 @@ export class AuthService {
         {
             observe: 'response',
             responseType: 'text'
-        })
+        });
+    }
+
+    refreshToken() {
+        return this.http.post(`${API_CONFIG.baseUrl}/auth/refresh_token`,
+        {},
+        {
+            observe: 'response',
+            responseType: 'text'
+        });
     }
 
     successfulLogin(authorizationValue : string) {
